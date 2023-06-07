091c21e0c58bc5e36cd68d291d0d8f0b59995b43
godriceeichie <emmaeichie@gmail.com>
2023-06-07 11:47:40 +0100

    Finished the hero and category section

--- carousel.js
+++ carousel.js
@@ -1,16 +1,57 @@
-import Swiper from 'swiper'
-import 'swiper/css'
+// // import Swiper from 'swiper'
+// // import 'swiper/css'
+
+// const swiper = new Swiper('.swiper', {
+//     // Optional parameters
+//     autoplay:{
+//         delay: 2000
+//     },
+
+//     direction: 'horizontal',
+
+//     loop: true,
+
+//     navigation: {
+//         nextEl: '.swiper-button-next',
+//         prevEl: '.swiper-button-prev',
+//     },
+
+//     effect: 'fade',
+  
+//     fadeEffect:{
+//         crossFade: true
+//     }
+// });
 
 const swiper = new Swiper('.swiper', {
-    // Optional parameters
-    autoplay:{
-        delay: 2000
+    autoplay: {
+      delay: 3000,
+      disableOnInteraction: false,
     },
-    direction: 'horizontal',
     loop: true,
-    effect: 'fade',
   
-    fadeEffect:{
-        crossFade: true
-    }
-});
+    // If we need pagination
+    pagination: {
+      el: '.swiper-pagination',
+      clickable: true,
+    },
+  
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-button-next',
+      prevEl: '.swiper-button-prev',
+    },
+
+    transition: {
+      name: 'carousel-animation',
+      duration: 300,
+      easing: 'ease-in-out'
+    },
+
+    scrollbar: {
+      el: '.swiper-scollbar',
+      hide: true,
+    },
+  });
+
+    
