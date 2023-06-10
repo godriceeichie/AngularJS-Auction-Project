0cd6ee38aea4d5c50802ab20face28c6ca7b6d78
godriceeichie <emmaeichie@gmail.com>
2023-06-10 18:17:46 +0100

    Finished the live auction section

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const angularModule = angular.module('angularModule', [])
 
 angularModule.controller('NavbarController', ['$scope', function($scope){
     $scope.message = "Hey World!"
+    $scope.date = new Date("2023-07-02")
+    
 
     $scope.isHamburgerOpen = false
     $scope.count = 0
@@ -38,6 +40,122 @@ angularModule.controller('NavbarController', ['$scope', function($scope){
             style: ''
         }
     ]
+
+}])
+
+
+angularModule.controller('CountdownController', ['$scope', '$interval', function($scope, $interval){
+
+   
+    // $scope.countDownDate = new Date("Oct 16, 2023 00:00:00").getTime();
+    // $scope.days
+    // $scope.hours
+    // $scope.minutes
+    // $scope.seconds
+
+    // $scope.getTimeInterval = function(){
+    //     $scope.now = new Date().getTime();
+    //     $scope.distance = $scope.countDownDate - $scope.now;
+
+    //     $scope.days = Math.floor($scope.distance / (1000 * 60 * 60 * 24));
+    //     $scope.hours = Math.floor(($scope.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    //     $scope.minutes = Math.floor(($scope.distance % (1000 * 60 * 60)) / (1000 * 60));
+    //     $scope.seconds = Math.floor(($scope.distance % (1000 * 60)) / 1000);
+        
+    // }; 
+
+
+
+    // $interval($scope.getTimeInterval, 1000)
+      
+    $scope.liveAuctionCard = [
+        {
+            name: "Alarm Clock 1990's",
+            currentBid: "739.0$",
+            imgPath: "images/time-antique-product.jpg",
+            endDate: new Date("Oct 16, 2023 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        },
+        {
+            name: "Premium 1998 Typewriter",
+            currentBid: "2,820,104.0$",
+            imgPath: "images/typewriter-antique-product.jpg",
+            endDate: new Date("Sep 1, 2023 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        },
+        {
+            name: "Macbook Pro 2018",
+            currentBid: "30,007.0$",
+            imgPath: "images/macbook-antique-product.jpg",
+            endDate: new Date("Feb 16, 2024 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        },
+        {
+            name: "Black Analogue Watch",
+            currentBid: "235.9$",
+            imgPath: "images/watch-antique-product.jpg",
+            endDate: new Date("July 2, 2023 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        },
+        {
+            name: "Ford Shelby White Car",
+            currentBid: "5,898.9$",
+            imgPath: "images/car-antique-product.jpg",
+            endDate: new Date("July 22, 2023 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        },
+        {
+            name: "Stylish Digital Airpods",
+            currentBid: "130.0$",
+            imgPath: "images/airpods-antique-products.jpg",
+            endDate: new Date("September 28, 2023 00:00:00").getTime(),
+            days: '',
+            hours: '',
+            minutes: '',
+            seconds: ''
+        }
+    ]
+
+    $scope.updateCountdown = function(){
+        $scope.now = new Date().getTime();
+        angular.forEach($scope.liveAuctionCard, function(liveAuctionCard){
+            $scope.endDate =  liveAuctionCard.endDate
+
+            
+            $scope.distance = $scope.endDate - $scope.now;
+
+            $scope.days = Math.floor($scope.distance / (1000 * 60 * 60 * 24));
+            $scope.hours = Math.floor(($scope.distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+            $scope.minutes = Math.floor(($scope.distance % (1000 * 60 * 60)) / (1000 * 60));
+            $scope.seconds = Math.floor(($scope.distance % (1000 * 60)) / 1000);
+
+            liveAuctionCard.days = $scope.days
+            liveAuctionCard.hours = $scope.hours
+            liveAuctionCard.minutes = $scope.minutes
+            liveAuctionCard.seconds = $scope.seconds
+
+        })
+    }
+
+    $scope.updateCountdown()
+
+    $interval($scope.updateCountdown, 1000)
+
 }])
 
 
---
+++ date.js
@@ -0,0 +1,28 @@
+const countDownDate = new Date("Oct 16, 2023 00:00:00").getTime();
+const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+
+const getTimeInterval = () => {
+  const now = new Date().getTime();
+  const distance = countDownDate - now;
+
+  const days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  const hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  const minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  const seconds = Math.floor((distance % (1000 * 60)) / 1000);
+  
+}
+
+// setInterval(getTimeInterval, 1000)
+
+const time = {
+  countdown: new Date("Oct 16, 2023 00:00:00").getTime(),
+  name: Math.floor(distance / (1000 * 60 * 60 * 24))
+}
+
+console.log(time)
+
+// console.log(getTimeInterval())
+
